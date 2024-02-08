a595adc1892510a97858a20bb31685bfd617bad4
DEBJIT PUROHIT <92798917+debjitpurohit@users.noreply.github.com>
2024-02-08 20:12:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 //deploying nodejs app
 app.use(cors({
     //within origin we pass the frontend url, backend only allow this allow
-    origin: 'https://agri-frontend-user-3qhnb6eqf-debjitpurohit.vercel.app',//must be pass though array 
+    origin: 'https://agri-frontend-user.vercel.app',//must be pass though array 
     // origin : 'https://agri-frontend-user-3qhnb6eqf-debjitpurohit.vercel.app',
     methods:["GET" , "POST","PUT","DELETE"],
     credentials: true,//by giving true backend allow cookies to give to the frontend
